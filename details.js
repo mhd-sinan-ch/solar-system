1b16cc0b79e0b14b0a6d2c7465147079a7c1ca93
Muhammed Sinan C H <mhdsinanch6235@gmail.com>
2026-01-16 21:20:01 +0530

    initial commit

---
+++ details.js
@@ -0,0 +1,30 @@
+function sun() {
+    window.location.href = "sun.html";
+}
+function mercury() {
+    window.location.href = "mercury.html";
+}
+function venus() {
+    window.location.href = "venus.html";
+}
+function earth() {
+    window.location.href = "earth.html";
+}
+function mars() {
+    window.location.href = "mars.html";
+}
+function jupiter() {
+    window.location.href = "jupiter.html";
+}
+function saturn() {
+    window.location.href = "Saturn.html";
+}
+function uranus() {
+    window.location.href = "Uranus.html";
+}
+function neptune() {
+    window.location.href = "Neptune.html";
+}
+function back() {
+    window.location.href = "front.html";
+}
